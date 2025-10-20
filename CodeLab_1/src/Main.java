316ddbb24f844a67e46eb68fa23ee051e3a5686d
MrMamang <farisramadhanr08@gmail.com>
2025-10-20 20:20:25 +0700

    saya me4nambahakan print ke dua

--- CodeLab_1/src/Main.java
+++ CodeLab_1/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("HALO ini poerubahan yagn sya alkukan");
     }
 }
